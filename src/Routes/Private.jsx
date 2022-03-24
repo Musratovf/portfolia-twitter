f3e2824574ff945d738d5b021cc0706fd455fa8a
kvmvkxnt <56192327+kvmvkxnt@users.noreply.github.com>
2022-03-24 16:52:44 +0500

    Update Private.jsx

--- src/Routes/Private.jsx
+++ src/Routes/Private.jsx
@@ -2,19 +2,19 @@ import {Outlet, Navigate, useLocation, NavLink} from 'react-router-dom'
 import useAuth from '../Hooks/UseAuth';
 import PrivateImg from '../img/private.jfif'
 import './Private.css'
-import useUser from '../Hooks/UseUser';
+
 
 
 function Private(){
     
     let [token] = useAuth()
-    let [user] = useUser()
+
     let {pathname} = useLocation()
  
-    if(token == 'bor'){
+    if(token === 'bor'){
         return <Outlet/> 
     }
-    if(token == 'otish mumkin'){
+    if(token === 'otish mumkin'){
         return (
             <div className="private__div">
                 <img className="private__img" src={PrivateImg} alt="img" />
@@ -26,9 +26,9 @@ function Private(){
             </div>
         )
     }
-    if(token == 'bor' && pathname == '/Login'){
+    if(token === 'bor' && pathname === '/Login'){
         return <Navigate to="/Home"/>
-    }else if(token == '' && pathname == '/Home'){
+    }else if(token === '' && pathname === '/Home'){
       return (
           <div className="private__div">
               <img className="private__img" src={PrivateImg} alt="img" />
